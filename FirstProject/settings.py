a0a1d347c1708c7a8e958ac6a6f6ae2e9cde5b3e
Mihir Kavatkar <kavatkar@usc.edu>
2016-08-30 11:53:54 -0700

    added models and combined it with project

--- FirstProject/settings.py
+++ FirstProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -85,18 +86,14 @@ DATABASES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator', },
 ]
 
 
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+
+# Database model for Questions
+@python_2_unicode_compatible
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    # Demo function to return datetime
+    def was_published_recently(self):
+        return self.pub_Date >= timezone.now() - datetime.timedelta(days=1)
+    question_text = models.CharField(max_length=200)
+    pub_Date = models.DateTimeField('date published')
+
+
+# Database model for choice
+@python_2_unicode_compatible
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
