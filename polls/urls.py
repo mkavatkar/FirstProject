e83e0da1ad6451bcad4f33cd4f3d0a2aa9613b0c
Mihir Kavatkar <kavatkar@usc.edu>
2016-08-30 12:12:55 -0700

    added sample views

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 # patterns matching url for the index/
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # ex. /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex. /polls/5/result/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex. /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse
 # index/ view for the polls app
 def index(request):
     return HttpResponse("Hello World, Location: index/")
+
+
+# Get the details of questions and display in this view
+def detail(request, question_id):
+    return HttpResponse("You are looking at Question %s" % question_id)
+
+
+# Result view
+def results(request, question_id):
+    response = "You are looking at the result of %s"
+    return HttpResponse(response % question_id)
+
+
+# Voting view
+def vote(request, question_id):
+    return HttpResponse("You are voting on Question %s" % question_id)
