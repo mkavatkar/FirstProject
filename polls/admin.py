14b7665877dc2099b79c90b404f1ae7da1cbebab
Mihir Kavatkar <kavatkar@usc.edu>
2016-08-31 10:53:56 -0700

    We are almost done here

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
+
+# Customise the admin form
+class ChoiceAdmin(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceAdmin]
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
